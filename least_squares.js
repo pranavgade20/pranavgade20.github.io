4a6c123b96a15587cc84a2fcb2ef73e4fde088bb
pranavgade20 <pranavgade20@gmail.com>
2017-05-31 13:57:41 +0530

    Add files via upload

---
+++ least_squares.js
@@ -0,0 +1,62 @@
+var xVals = [];
+var yVals = [];
+var xSum = 0;
+var ySum = 0;
+
+function setup(){
+   createCanvas(windowWidth, windowHeight);
+   background(95, 154, 76);
+   noLoop();
+}
+
+function draw(){
+   background(95, 154, 76);
+
+   var xMean = xSum/xVals.length;
+   var yMean = ySum/yVals.length;
+   var numer = 0;
+   var denom = 0;
+
+   for (var i = 0; i < xVals.length; i++) {
+      stroke(170, 255, 0);
+      strokeWeight(3);
+      point(xVals[i], yVals[i]);//show the points
+      strokeWeight(1);
+      noFill();
+      ellipse(xVals[i], yVals[i], 10, 10);//make it fancier
+
+      //the ordinary least squares method to find the line
+      numer += (xVals[i]-xMean)*(yVals[i]-yMean);
+      denom += (xVals[i]-xMean)*(xVals[i]-xMean);
+   }
+
+   var m = numer/denom;
+   var b = yMean - (m*xMean);
+
+   var y1 = b;
+   var y2 = (m*width) + b;
+
+   stroke(0, 0, 225);
+   strokeWeight(3);
+   line(0, y1, width, y2);
+
+   //display the error rate
+   var errSum = 0;
+   for (var i = 0; i < xVals.length; i++) {
+      errSum += (yVals[i] -(m*xVals[i] + b)) * (yVals[i] -(m*xVals[i] + b));
+   }
+   errSum /= xVals.length;
+   stroke(170, 255, 0);
+   strokeWeight(1);
+   text(round(errSum), 20, 20);
+}
+
+function mousePressed(){
+   xVals.push(round(mouseX));
+   yVals.push(round(mouseY));
+
+   xSum+=xVals[xVals.length-1];
+   ySum+=yVals[yVals.length-1];
+
+   redraw();
+}
